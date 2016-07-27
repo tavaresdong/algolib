14316dddfb4552a3479425444206704678dee8b6
tavaresdong <tavares.nju@gmail.com>
2016-07-27 09:22:28 +0800

    simultaneous min max

--- algorithms_cpp/algorithms_cpp/algo/Selection.h
+++ algorithms_cpp/algorithms_cpp/algo/Selection.h
@@ -1,8 +1,10 @@
 #pragma once
 #ifndef _SELECTION_H_
 #define _SELECTION_H_
+
 #include <iterator>
 #include <functional>
+#include <utility>
 
 namespace algo
 {
@@ -30,11 +32,78 @@ namespace algo
         return __select_winner(first, last, std::less<T>());
     }
 
-        template <typename RandomIter>
+    template <typename RandomIter>
     RandomIter max(RandomIter first, RandomIter last)
     {
         typedef typename std::iterator_traits<RandomIter>::value_type T;
         return __select_winner(first, last, std::greater<T>());
+    }
+
+    
+    // An algorithm to get min and max simultaneously with 3/2*n comparison
+    template <typename RandomIter>
+    std::pair<RandomIter, RandomIter> minmax(RandomIter first, RandomIter last)
+    {
+        typedef typename std::iterator_traits<RandomIter>::value_type T;
+        RandomIter big, small;
+        RandomIter p1, p2;
+        auto dist = std::distance(first, last);
+        std::less<T> comp;
+
+        if (first == last)
+        {
+            return std::pair<RandomIter, RandomIter>(first, first);
+
+        }
+
+        if (dist % 2 != 0)
+        {
+            big = small = first;
+            p1 = next(first);
+        }
+        else
+        {
+            RandomIter n = next(first);
+            if (comp(*first, *n))
+            {
+                big = n;
+                small = first;
+            }
+            else
+            {
+                big = first;
+                small = n;
+            }
+            p1 = next(n);
+        }
+
+        for (; p1 != last; p1 = next(p2))
+        {
+            p2 = std::next(p1);
+            if (comp(*p1, *p2)) 
+            {
+                if (comp(*p1, *small))
+                {
+                    small = p1;
+                }
+                if (comp(*big, *p2))
+                {
+                    big = p2;
+                }
+            }
+            else
+            {
+                if (comp(*p2, *small))
+                {
+                    small = p2;
+                }
+                if (comp(*big, *p1))
+                {
+                    big = p1;
+                }
+            }
+        }
+        return std::pair<RandomIter, RandomIter>(small, big);
     }
 }
 #endif
--- algorithms_cpp/algorithms_cpp/algo/test/selection_test.cpp
+++ algorithms_cpp/algorithms_cpp/algo/test/selection_test.cpp
@@ -9,11 +9,16 @@ namespace algo
 {
     void minmax_test()
     {
-        std::vector<int> v = { 5, 4, 6, 10, 12, 3, 7 };
+        std::vector<int> v = { 5, 4, 6, 10, 12, 3, 7, 9 };
         std::cout << *min(v.begin(), v.end()) << std::endl;
 
         std::vector<std::string> v2 = { "hello", "hellowordl", "hellowordz", "daydream", "das~~" };
         std::cout << *max(v2.begin(), v2.end()) << std::endl;
+
+        auto res = minmax(v2.begin(), v2.end());
+        auto res2 = minmax(v.begin(), v.end());
+        std::cout << *res.first << *res.second << std::endl;
+        std::cout << *res2.first << *res2.second << std::endl;
     }
 
 }
